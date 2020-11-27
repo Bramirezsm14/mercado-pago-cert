f1a0e2495a874153cee77dabc96be49c8503ee23
Bramirez <bramirezsm14@gmail.com>
2020-11-27 17:32:02 -0300

    mercadolibre

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const indexController = require("../controllers/indexController");
+
+/* GET home page. */
+router.get('/', indexController.home);
+
+/* GET detail page */
+router.get('/detail', indexController.detail);
+
+router.get("/success", indexController.success);
+
+router.get("/failure", indexController.failure);
+
+router.get("/pending", indexController.pending);
+
+router.post("/notifications", indexController.notifications)
+
+module.exports = router;
